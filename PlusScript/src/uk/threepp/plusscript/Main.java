37a33fe96aa4814f489e414d22075ba0391ffbc1
Oliver Wilshaw <oli.wilshaw@gmail.com>
2015-07-10 15:58:08 +0100

    Update Plugins

--- PlusScript/src/uk/threepp/plusscript/Main.java
+++ PlusScript/src/uk/threepp/plusscript/Main.java
@@ -1,11 +1,16 @@
 package uk.threepp.plusscript;
 
+import uk.threepp.plusscript.input.TextHandler;
+
 public class Main {
 
 	private static String banner;
+	private static String fname;
+	private static char[] fileContents;
+	private static TextHandler textHandler;
 	
-	public static void main(String[] args) {
-
+	public static void main(String... args) {
+		
 		banner = "###########################"
 				+ "\n\n"
 				+ "PlusScript Interpreter v1.0"
@@ -13,6 +18,20 @@ public class Main {
 				+ "\n\n"
 				+ "###########################";
 		
+		fname = "";
+		for (String s : args) {
+			
+			fname += s + " ";
+		}
+		
 		System.out.println(banner);
+		
+		textHandler = new TextHandler();
+		
+		if (fname != "") {
+			
+			fileContents = textHandler.cacheFile(fname);
+			if (fileContents != null) System.out.println(fileContents);
+		}
 	}
 }
